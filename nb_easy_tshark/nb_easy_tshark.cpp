49980f1181db9d2e8ad44d1dab6e86ebf4f69152
yangnanbei <406631132@qq.com>
2025-08-16 23:39:20 +0800

    Improvement, start up, get pcap data from tshark, yang_nanbei

--- nb_easy_tshark/nb_easy_tshark.cpp
+++ nb_easy_tshark/nb_easy_tshark.cpp
@@ -1,8 +1,22 @@
 ﻿#include <iostream>
-#include "httplib/httplib.h"
+#include <cstdio>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const char* read_pcap_cmd = "tshark -r E:/Proj/nb_easy_tshark/pcap/capture.pcap";
+    std::cerr << "cmd is " << read_pcap_cmd << std::endl;
+    FILE *pipe = _popen(read_pcap_cmd, "r");
+    if (!pipe) {
+        std::cerr << "Fail to read pcap by tshark!" << std::endl;
+        return 1;
+    }
+
+    char buffer[4096];
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        std::cout << buffer;
+    }
+    std::cout << std::endl;
+    _pclose(pipe);
+    return 0;
 }
 
